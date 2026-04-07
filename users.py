c541dcf91f7e3da2637cfb6bdc3ece4c23e72d33
nahidkhan08 <khan15-5435@diu.edu.bd>
2026-04-07 19:12:12 +0600

    Updated code for some redundancy

--- users.py
+++ users.py
@@ -21,19 +21,34 @@ class Employee(User):
 class Admin(User):
     def __init__(self, name, phone,email, address):
         super().__init__(name, phone, email, address)
-        self.employees=[]
     
-    def add_employee(self, name, phone, email, address, age, designation, salary):
-        employee= Employee(name, phone, email, address, age, designation, salary)
+    def add_employee(self, restaurant, employee):
+        restaurant.add_employee(employee)
+    
+    def view_employee(self, restaurant):
+        restaurant.view_employee()
+    
+    
+
+class Restaurant:
+    def __init__(self, name):
+        self.name=name
+        self.employees=[] #this part will work like a database
+    
+    def add_employee(self, employee):
         self.employees.append(employee)
-        print(f'{name} is added!!!')
+        print(f'{employee.name} is added!!!')
     
     def view_employee(self):
-        print("Employee Information:")
+        print("Employee List:")
         for emp in self.employees:
             print(f'Name: {emp.name}, Phone: {emp.phone}, Email: {emp.email}, Address: {emp.address}')
 
+        
+
 adm = Admin("Rahim", '123456789', 'rahim@gmail.com', 'Dhaka')
-adm.add_employee('Raj', '12349828', 'raj23@gmail.com', 'Savar', '23', 'Sales', '21000')
-adm.view_employee()
+restaurant=Restaurant("Road Side Kitchen")
+em=Employee('Raj', '12349828', 'raj23@gmail.com', 'Savar', '23', 'Sales', '21000')
+adm.add_employee(restaurant,em)
+
 
